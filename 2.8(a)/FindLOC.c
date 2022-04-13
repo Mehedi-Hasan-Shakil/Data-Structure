c8b8bc596c76a7a9cf091428fc8b2170798fb792
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-04-13 18:12:28 +0600

    Location of largest and second largest element.

---
+++ 2.8(a)/FindLOC.c
@@ -0,0 +1,29 @@
+/*Write a procedure FIND(DATA, N, LOC1, LOC2) which finds
+the location LOC1 of the largest element and the location
+LOC2 of the second largest element in an array DATA with n
+> 1 elements.*/
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {3, 9, 2, 4, 7};
+    int largest = arr[0], slargest = arr[0], LOC1 = 0, LOC2 = 0;
+    for(int i = 1; i < 5; i++)
+    {
+        if(largest < arr[i])
+        {
+            slargest = largest;
+            largest = arr[i];
+            LOC2 = LOC1;
+            LOC1 = i;
+        }
+        else if(slargest < arr[i])
+        {
+            slargest = arr[i];
+            LOC2 = i;
+        }
+    }
+    printf("Largest = %d Second Largest = %d\n", largest, slargest);
+    printf("Largest LOC = %d Second Largest LOC = %d\n", LOC1, LOC2);
+    return 0;
+}
