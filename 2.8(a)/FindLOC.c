122586b46e7b078ef2f5784a82c79e7a0f4b95f5
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-04-13 20:02:37 +0600

    Find largest and 2nd largest element & LOC

--- 2.8(a)/FindLOC.c
+++ 2.8(a)/FindLOC.c
@@ -7,20 +7,28 @@ LOC2 of the second largest element in an array DATA with n
 
 int main()
 {
-    int arr[] = {3, 9, 2, 4, 7};
-    int largest = arr[0], slargest = arr[0], LOC1 = 0, LOC2 = 0;
-    for(int i = 1; i < 5; i++)
+    int arr[] = {3, 9, 3, 4, 7};
+    int largest = arr[0], slargest = arr[1], LOC1 = 0, LOC2 = 1;
+    if(slargest > largest) {
+        int tmp = largest;
+        largest = slargest;
+        slargest = tmp;
+        tmp = LOC1;
+        LOC1 = LOC2;
+        LOC2 = tmp;
+    }
+    for(int i = 2; i < 5; i++)
     {
         if(largest < arr[i])
         {
-            slargest = largest; //When a largest element is found, the previous
-            largest = arr[i];   //largest element is transfered to the second
-            LOC2 = LOC1;        //largest variable as well as its location
+            slargest = largest;
+            largest = arr[i];
+            LOC2 = LOC1;
             LOC1 = i;
         }
         else if(slargest < arr[i])
         {
-            slargest = arr[i];  //Checks for second largest element only
+            slargest = arr[i];
             LOC2 = i;
         }
     }
@@ -28,3 +36,4 @@ int main()
     printf("Largest LOC = %d Second Largest LOC = %d\n", LOC1, LOC2);
     return 0;
 }
+
