6323e845da80a9a101509d41fa9757008e457f5f
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-08-09 19:16:58 +0600

    Solution of question 1 & 2

--- LabExercies/Graph/powers_of_matrix.c
+++ LabExercies/Graph/powers_of_matrix.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int power_matrix[20][20][20];
+int all_length_matrix[20][20];
+int path_matrix[20][20];
+int is_strongly_connected = 1;
 int main(void)
 {
     int n;
-    printf("Enter number of nodes : ");
+    printf("Enter number of nodes: ");
     scanf("%d", &n);
     int matrix[n][n];
-    printf("Enter your adjacency matrix :\n");
+    printf("Enter your adjacency matrix:\n");
     for (int i = 0; i < n; i++)
     { // take input as matrix
         for (int j = 0; j < n; j++)
@@ -34,11 +37,11 @@ int main(void)
             {
                 if (i == 0) // copy adjacency matrix to power matrix[0][n][n]
                 {
-                    for (int i = 0; i < n; i++)
+                    for (int p = 0; p < n; p++)
                     {
-                        for (int j = 0; j < n; j++)
+                        for (int q = 0; q < n; q++)
                         {
-                            power_matrix[0][i][j] = matrix[i][j];
+                            power_matrix[0][p][q] = matrix[p][q];
                         }
                     }
                 }
@@ -47,7 +50,7 @@ int main(void)
                     int sum = 0;
                     for (int l = 0; l < n; l++)
                     {
-                        sum += power_matrix[i - 1][j][l] * matrix[l][k];
+                        sum += power_matrix[i - 1][j][l] * matrix[l][k]; // matrix multiplication
                     }
                     power_matrix[i][j][k] = sum;
                 }
@@ -60,6 +63,7 @@ int main(void)
                 if (power_matrix[i][p][q])
                 {
                     printf("%d path(s) between %d and %d of length %d\n", power_matrix[i][p][q], p, q, i + 1);
+                    all_length_matrix[p][q] += power_matrix[i][p][q]; // creating all length matrix
                 }
                 else
                 {
@@ -69,5 +73,42 @@ int main(void)
         }
         printf("\n\n");
     }
+    printf("All length matrix:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", all_length_matrix[i][j]);
+            if (all_length_matrix[i][j]) // creating path matrix
+            {
+                path_matrix[i][j] = 1;
+            }
+            if (all_length_matrix[i][j] == 0 && is_strongly_connected)
+            {
+                is_strongly_connected = 0;
+            }
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+    printf("Path matrix:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", path_matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+    if (is_strongly_connected)
+    {
+        printf("The graph is strongly connected.");
+    }
+    else
+    {
+        printf("The graph is not connected strongly.");
+    }
+    printf("\n\n");
     return 0;
 }
